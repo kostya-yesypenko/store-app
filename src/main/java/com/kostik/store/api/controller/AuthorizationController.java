d5cbbdbf7059eb90d1ef604234713eb0b09f686c
Kostia <kostaesipenko120@gmail.com>
2024-08-10 14:50:43 +0300

    update

--- src/main/java/com/kostik/store/api/controller/AuthorizationController.java
+++ src/main/java/com/kostik/store/api/controller/AuthorizationController.java
@@ -1,7 +1,6 @@
 package com.kostik.store.api.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,9 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kostik.store.domain.User;
 import com.kostik.store.dto.AuthorizationRequest;
 import com.kostik.store.service.AuthorizationService;
@@ -22,10 +19,8 @@ import com.kostik.store.service.CartService;
 import com.kostik.store.service.HttpService;
 import com.kostik.store.service.UserService;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -51,25 +46,34 @@ public class AuthorizationController {
     public ResponseEntity<User> login(HttpServletRequest httpRequest, HttpServletResponse httpResponse, @RequestBody AuthorizationRequest request) {
         String login = request.getLogin();
         String password = request.getPassword();
-        User user = userService.findByLogin(login);
+        Optional<User> user = userService.findByLogin(login);
 
-        if (user != null && authorizationService.validatePasswords(password, user.getPassword())) {
-        	httpService.printCurrentSession();
-        	
-        	httpService.saveCurrentUserInSession(user);            
-            
-            cartService.loadCart(user);
+        // Check if user is present and the passwords match
+        if (user.isPresent() && authorizationService.validatePasswords(password, user.get().getPassword())) {
+            // Print current session for debugging
+            httpService.printCurrentSession();
+
+            // Save the current user in session
+            httpService.saveCurrentUserInSession(user.get());            
+
+            // Load the user's cart
+            cartService.loadCart(user.get());
+
+            // Log and return the user
             System.out.println("User set in session: " + user);
-            return new ResponseEntity<>(user, HttpStatus.OK);
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
         }
+
+        // Log and return a not found response if login is invalid
         System.out.println("Invalid login attempt for login: " + login);
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);        
     }
 
 
-//	@PostMapping("/logout")
-//	public void logout(@RequestParam @NotEmpty String refreshToken) {
-//		tokenService.delete(tokenService.findByRefreshToken(refreshToken));
-//	}
+
+	@PostMapping("/logout")
+	public void logout(User user) {
+		
+	}
 	
 }
--- src/main/java/com/kostik/store/service/OrderService.java
+++ src/main/java/com/kostik/store/service/OrderService.java
@@ -1,21 +1,19 @@
 package com.kostik.store.service;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import com.kostik.store.domain.Cart;
 import com.kostik.store.domain.Order;
 import com.kostik.store.domain.Product;
 import com.kostik.store.domain.User;
 import com.kostik.store.repository.OrderRepository;
-import com.kostik.store.repository.ProductRepository;
-import com.kostik.store.repository.UserRepository;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
@@ -26,49 +24,46 @@ import lombok.extern.slf4j.Slf4j;
 public class OrderService {
 
 	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private ProductRepository productRepository;
-
-	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private DataSource ds;
-	
+
 	@Autowired
-	private JdbcTemplate jdbcTemplate;	
-		
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private ProductService productService;
+
 	@PostConstruct
 	void init() {
 		log.debug("Order service bean has been constructed");
 	}
-	
-	public Order createOrder(User user, Long productId, Long quantity, Double price) throws UserNotFoundException, ProductNotFoundException {		
-		// Find the product by productId
-		Product product = productRepository.findById(productId).orElse(null);
+
+	public Order createOrder(User user, Long productId, Long quantity, Double price) throws UserNotFoundException, ProductNotFoundException, Exception {
+		Product product = productService.getProductById(productId);
 
 		if (product == null) {
 			throw new ProductNotFoundException("Product not found with ID: " + productId);
 		}
 
+		if(quantity>product.getQty())
+			throw new Exception("Not enough products on market");
 		// Create a new OrderEntity
 		Order order = new Order();
 		order.setUser(user);
 		order.setProduct(product);
 		order.setQty(quantity);
 		order.setPrice(price);
-				
+
 		return order;
 	}
-	
-
 
 	@Transactional
 	public Order saveOrder(Order order) throws Exception {
+		order.setDateModified(LocalDateTime.now());
 		Order dbOrder = orderRepository.save(order);
-		
+
 		if (dbOrder == null) {
 			throw new Exception("Can not create order");
 		}
@@ -80,10 +75,10 @@ public class OrderService {
 		}
 		// Update product quantity by subtracting order quantity
 		product.setQty(product.getQty() - order.getQty());
-		productRepository.save(product);
+		productService.save(product);
 		return dbOrder;
 	}
-	
+
 	public List<Order> getOrders() {
 		return (orderRepository.findAll());
 	}
@@ -99,7 +94,5 @@ public class OrderService {
 			super(message);
 		}
 	}
-
-
 
 }
